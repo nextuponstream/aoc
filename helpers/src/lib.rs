b6baf733946cb15ccb34a7871a1ea9be8c2bb294
nextuponstream <nextuponstream@gmail.com>
2023-12-02 10:34:03 +0100

    Add test

--- helpers/src/lib.rs
+++ helpers/src/lib.rs
@@ -1,3 +1,21 @@
 pub fn hello_world() -> String {
     String::from("Hello world from helper")
 }
+
+use regex::Regex;
+
+pub fn parse_first_digit(input: &str) -> u32 {
+    0
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    #[test]
+    fn get_easy() {
+        let input = "1abc";
+        let result = parse_first_digit(input);
+        assert_eq!(result, 1)
+    }
+}
