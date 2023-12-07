603f2ed3c9bbc23f8ff7d127be9630d9f184fb6d
nextuponstream <nextuponstream@gmail.com>
2023-12-07 14:57:20 +0100

    WIP

--- day-05/src/main.rs
+++ day-05/src/main.rs
@@ -14,7 +14,19 @@ struct Almanac {
 
 impl Almanac {
     fn new(input: String) -> Self {
-        Self::default()
+        let sections: Vec<&str> = input.split("\n\n").collect();
+        println!("{sections:?}");
+        let seeds_input = sections[0];
+        let seeds_input = seeds_input.split_once(':').unwrap().1;
+        let seeds_numbers: Vec<&str> = seeds_input.split_whitespace().collect();
+        let mut seeds: Vec<u32> = vec![];
+        for seed in seeds_numbers {
+            seeds.push(seed.parse().unwrap())
+        }
+        Self {
+            seeds,
+            maps: vec![],
+        }
     }
 
     fn lowest_location_number(&self) -> u32 {
@@ -24,7 +36,11 @@ impl Almanac {
 
 fn main() {
     let input = get_input();
-    println!("???");
+    let almanac = Almanac::new(input);
+    println!(
+        "lowest location number: {}",
+        almanac.lowest_location_number()
+    );
 }
 
 #[cfg(test)]
@@ -33,7 +49,48 @@ mod tests {
 
     #[test]
     fn almanac_example_1() {
-        let almanac = Almanac::new(String::default());
+        let almanac = Almanac::new(
+            "seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+"
+            .into(),
+        );
+        assert!(almanac.seeds.contains(&79), "{almanac:?}");
+        assert!(almanac.seeds.contains(&14), "{almanac:?}");
+        assert!(almanac.seeds.contains(&55), "{almanac:?}");
+        assert!(almanac.seeds.contains(&13), "{almanac:?}");
+        assert_eq!(almanac.seeds.len(), 4, "{almanac:?}");
         assert_eq!(almanac.lowest_location_number(), 35, "{almanac:?}")
     }
 }
